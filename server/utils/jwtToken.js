0dd29c0196e0bd77ddcd73891040f08cc0eaa89b
Himanshu Sahu <himpreetak@gmail.com>
2025-09-12 19:25:19 +0530

    sdjfl

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -15,7 +15,7 @@ export const sendToken = (res, user) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: "strict",
+    sameSite: isProduction ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
   return token;
